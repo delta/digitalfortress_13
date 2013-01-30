3d23b05186a3949226a2762bbfbf72b1be8019b1
vignesh <vignesh@vignesh-ThinkPad-L412.(none)>
2013-01-30 22:37:07 +0530

    initial commit

---
+++ web/task1/index.php
@@ -0,0 +1,26 @@
+<?php
+if(isset($_REQUEST['submit'])){
+	$connection = mysql_connect("server","user","password");
+	mysql_select_db($database_name);
+	
+	$query = "SELECT * FROM `task1` WHERE `password` LIKE ";
+	$result = mysql_query($query);
+	if(mysql_fetch_array($result)){
+		
+	}
+}
+?>
+
+<html>
+	<head>
+		<title>Welcome to the CTF!</title>
+	</head>
+	<body>
+		<center>task1</center>
+		<form method="get" action="">
+			<input type="text" name="user" />
+			<input type="password" name="password" />
+			<input type="submit" name="submit" value="submit"/>
+		</form>
+	</body>
+</html>
