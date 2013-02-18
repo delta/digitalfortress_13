fe06ee12a28760ddfa5c0185c8083c4a2260cc07
nobelium <vignesh_goldfish@yahoo.com>
2013-02-18 16:11:22 +0530

    adding main htaccess

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Routes all request to their pages
+ * 
+ * Make sure they cannot be reached directly..
+ * 
+ */
+ 
+ 
+
+echo "HOST : ".$_SERVER['HTTP_HOST']."  SELF: ".$_SERVER['PHP_SELF'];
+print_r($_GET); 
+?>
