3f9548923d3550d598e4dfefcbf30b527844d9f6
nobelium <vignesh_goldfish@yahoo.com>
2013-02-19 22:12:49 +0530

    navigation done

--- pages/main/home.php
+++ pages/main/home.php
@@ -1,12 +1,34 @@
-<pre>
-Digital fortress		    __           _                     
- ____  _        _  _          _    / _|         | |                    
-|  _ \(_) __ _ (_)| |_  __ _ | |  | |_ ___  _ __| |_ _ __ ___  ___ ___ 
-| | \ | |/ _' || || __|/ _' || |  |  _/ _ \| '__| __| '__/ _ \/ __/ __|
-| |_/ | | (_| || || |_( (_| || |  | || (_) | |  | |_| | |  __/\__ \__ \
-|____/|_|\__. ||_|\___|\__._||_|  |_| \___/|_|   \__|_|  \___||___/___/
-          / | |			
-         / (| |			
-         \____/                                          
-                                
-</pre>                                                                                  
+<html>
+	<head>
+		<title>Digital Fortress</title>
+		<style type="text/css">
+			
+		</style>
+	</head>
+	<body>
+		<div id="maintitle">
+			<pre>
+			                                    __           _                     
+			 ____  _        _  _          _    / _|         | |                    
+			|  _ \(_) __ _ (_)| |_  __ _ | |  | |_ ___  _ __| |_ _ __ ___  ___ ___ 
+			| | \ | |/ _' || || __|/ _' || |  |  _/ _ \| '__| __| '__/ _ \/ __/ __|
+			| |_/ | | (_| || || |_( (_| || |  | || (_) | |  | |_| | |  __/\__ \__ \
+			|____/|_|\__. ||_|\___|\__._||_|  |_| \___/|_|   \__|_|  \___||___/___/
+			          / | |			
+			         / (| |			
+			         \____/                                          
+			                                
+			</pre>                                                                                  
+		</div>
+		<div id="content">
+			<p>Greetings!!! Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text  
+				Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text 
+				Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text 
+				Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text Some_text </p>
+			<div>Link to irc channel</div>
+			<div>link to <a href="/stats">stats page</a></div>
+			<div>link to <a href="/dashboard">questions</a></div>
+			<div>link to <a href="/submit">submit page</a></div>
+		</div>
+	</body>
+</html>
--- web_root/index.php
+++ web_root/index.php
@@ -14,24 +14,29 @@ print_r($_GET);
 
 $req_path = $_GET['query'];
 
-$routes = array("web/task1"=>"/pages/web/task1/index.php",
-				"web/task2"=>"/pages/web/task1/index.php",
-				"web/task3"=>"/pages/web/task1/index.php",
-				"web/task4"=>"/pages/web/task1/index.php",
-				"web/task5"=>"/pages/web/task1/index.php",
-				"crypto/task1"=>"/pages/crypto/task1/index.php",
-				"crypto/task2"=>"/pages/crypto/task1/index.php",
-				"crypto/task3"=>"/pages/crypto/task1/index.php",
-				"crypto/task4"=>"/pages/crypto/task1/index.php",
-				"crypto/task5"=>"/pages/crypto/task1/index.php",
-				"stats"=>"/pages/main/stats.php",
-				"dashboard"=>"/pages/main/dashboard.php",
-				"submit"=>"/pages/main/submit.php");
-				
-if($req_path=="" || empty($req_path)){
-	require_once '';
-}			
-if(file_exists(__DIR__."/../".$routes[$req_path])){
-	require_once __DIR__."/../".$routes[$req_path]; 
+$routes = array("web/task1/"=>"/pages/web/task1/index.php",
+				"web/task2/"=>"/pages/web/task1/index.php",
+				"web/task3/"=>"/pages/web/task1/index.php",
+				"web/task4/"=>"/pages/web/task1/index.php",
+				"web/task5/"=>"/pages/web/task1/index.php",
+				"crypto/task1/"=>"/pages/crypto/task1/index.php",
+				"crypto/task2/"=>"/pages/crypto/task1/index.php",
+				"crypto/task3/"=>"/pages/crypto/task1/index.php",
+				"crypto/task4/"=>"/pages/crypto/task1/index.php",
+				"crypto/task5/"=>"/pages/crypto/task1/index.php",
+				"stats/"=>"/pages/main/stats.php",
+				"dashboard/"=>"/pages/main/dashboard.php",
+				"submit/"=>"/pages/main/submit.php");
+
+
+if($req_path=="" || empty($req_path) || !isset($_GET['query'])){
+	require_once __DIR__."/../pages/main/home.php";
+} else if(isset($routes[$req_path]) && file_exists(__DIR__."/..".$routes[$req_path])){
+	require_once __DIR__."/..".$routes[$req_path]; 
+} else if(isset($routes[$req_path."/"]) && file_exists(__DIR__."/..".$routes[$req_path."/"])){
+	require_once __DIR__."/..".$routes[$req_path."/"]; 
+} else {
+	echo "Oops!! 404, File not found";
 }
+
 ?>
