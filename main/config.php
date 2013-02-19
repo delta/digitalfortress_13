8a08f5434d2800f57ce7241aa1a8bb1b55a2f2b9
paramesh waran <hsemarap@gmail.com>
2013-02-19 13:02:52 +0530

    db dump

---
+++ main/config.php
@@ -0,0 +1,16 @@
+<?php
+			$host="localhost";
+			$user="paramesh";
+			$pass="pass";
+			$db  ="digitalfortress";
+			$user_table="users";
+			$score_table="scores";
+			$ans_table="ans";
+			$categories=array("web","trivia","crypto");
+			$scores=array(
+					array(10,20,30,40),
+					array(10,20,30,40),
+					array(10,20,30,40)
+				     );
+			session_start();
+?>
