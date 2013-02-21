edce780efac604ef18fd09ab514dc13763189133
paramesh waran <hsemarap@gmail.com>
2013-02-21 12:30:50 +0530

    changed Crypto,web4 form as table

--- pages/crypto/task2/index.php
+++ pages/crypto/task2/index.php
@@ -11,15 +11,21 @@ if(isset($_REQUEST['submit'])){
 <html>
 	<head>
 		<title></title>
+		<link rel="stylesheet" href="/css/template.css" type="text/css"/>
 	</head>
 	<body>
+		<?php require_once "header.php";?>
 		<center>crypto task2</center>
 		<p>DERP now has changed the encryption algorithm and it uses onetime pad encryption to encrypt the password. 
 			You have found the encrypted password. You have access to the machine that encrypts the password</p>
 		<p>Derps encrypted password is "andanoobpassword"(quotes for clarity). The key concatenated with the password is the answer for this level</p>
-		<form>
-			<input type="text" name="pass"/>
-			<input type="submit" name="submit" value="submit"/>
+		<form method="get" action="">
+				<table>
+					<tr>
+						<td><input type="text" name="pass" /></td>
+						<td><input type="submit" name="submit" value="submit"/></td>
+					</tr>
+				</table>
 		</form>
 		<?php
 			if(isset($string) && !empty($string))echo "The encrypted string is :  ".$string;
