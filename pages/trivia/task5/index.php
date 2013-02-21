5fb07bba6871c0383ed776126780c4993cbbf553
nobelium <vignesh_goldfish@yahoo.com>
2013-02-21 12:40:21 +0530

    adding trivia

--- pages/trivia/task5/index.php
+++ pages/trivia/task5/index.php
@@ -2,8 +2,12 @@
 if(isset($_REQUEST['submit'])){
 	if(isset($_REQUEST['ans'])){
 		$ans = stripslashes($_REQUEST['ans']);
+		$flag =1;
 		if(md5($ans)==="md5ans"){
 			$ans = "level password";
+		} else {
+			$flag = 0;
+			$ans = "wrong answer";
 		}
 	}
 }
@@ -11,20 +15,41 @@ if(isset($_REQUEST['submit'])){
 <html>
 	<head>
 		<title>title</title>
+		<link rel="stylesheet" href="/css/template.css" type="text/css"/>
 	</head>
 	<body>
-		<center>title</center>
-		Question for this round
-		<form method="get" action="">
-			<input type="text" name="ans" />
-			<input type="submit" name="submit" value="submit" />
-		</form>
-		<?php
-			if(isset($_REQUEST['ans'])){
-				echo $ans;
-			} else if(!isset($_REQUEST['ans'])){
-				echo "wrong ans";
-			}
-		?>
+		<?php require_once "header.php";?>
+		<div id="contents">
+			<center>Trivia-task-5</center>
+			<div id="question">Question question question question question question question question question question question
+				 question question question question question question question question question question question question question
+				  question question question question question question question question question question question question question
+				   question question question question question question question question question question question question
+		   </div>
+			
+			<?php
+				if(isset($_REQUEST['ans']) && $flag){
+					echo $ans;
+				} else if(!isset($_REQUEST['ans']) || !$flag){
+					if(isset($_REQUEST['ans']))echo "<div align='center' class='center' style='padding-top:20px;'>Wrong ans</div>";
+					?>
+			<div align="center">
+				<form action="" method="get" enctype="multipart/form-data">
+					<table>
+						<tr>
+							<td><label for="ans">Your answer : </label></td>
+							<td><input type="text" name="ans" /></td>
+						</tr>
+						<tr>
+							<td></td>
+							<td><input type="submit" name="submit" value="submit" /></td>
+						</tr>
+					</table>
+				</form>
+			</div>
+			<?php
+				}
+			?>
+		</div>
 	</body>
 </html>
