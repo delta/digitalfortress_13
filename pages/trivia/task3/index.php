a68ed6df56844122fcb247c359f394ea833f7f90
nobelium <vignesh_goldfish@yahoo.com>
2013-02-22 15:34:44 +0530

    adding trivia 3

--- pages/trivia/task3/index.php
+++ pages/trivia/task3/index.php
@@ -1,9 +1,10 @@
 <?php
+//ans is TORUSINTERCONNECT
 if(isset($_REQUEST['submit'])){
 	if(isset($_REQUEST['ans'])){
 		$ans = stripslashes($_REQUEST['ans']);
 		$flag =1;
-		if(md5($ans)==="md5ans"){
+		if(md5($ans)==="17fc663f850af56b09cfa20b56884f8f"){
 			$ans = "level password";
 		} else {
 			$flag = 0;
@@ -21,15 +22,12 @@ if(isset($_REQUEST['submit'])){
 		<?php require_once "header.php";?>
 		<div id="contents">
 			<center>Trivia-task-3</center>
-			<div id="question">Question question question question question question question question question question question
-				 question question question question question question question question question question question question question
-				  question question question question question question question question question question question question question
-				   question question question question question question question question question question question question
-		   </div>
+			<div id="question">Network topology -> parallel computers</div>
+			<div>Note: Answers consists only of uppercase letters and do not contain special characters or white spaces..</div>
 			
 			<?php
 				if(isset($_REQUEST['ans']) && $flag){
-					echo $ans;
+					echo "<br/><br/><div align='center'>LEVEL PASSWORD: ".$ans."</div>";
 				} else if(!isset($_REQUEST['ans']) || !$flag){
 					if(isset($_REQUEST['ans']))echo "<div align='center' class='center' style='padding-top:20px;'>Wrong ans</div>";
 					?>
