7d2d086acef04ca6d2906d3f90fc7e6e0761d6de
Deepak Raj <hop2deep@gmail.com>
2013-02-24 18:18:54 +0530

    task2 created

---
+++ pages/web/task2/index.php
@@ -0,0 +1,28 @@
+<?php
+        
+?>
+<html>
+<head>
+<title>Task2</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta name="robots" content="noindex" />
+
+<style>
+	#log, #pass { color: white; background-color:black; padding-left:8px; padding-right:8px; }
+
+</style>
+</head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
+<body bgcolor="#FFFFFF">
+
+<h2>"HEY" </h2>
+<p><b>"hello" </b> <span><?php echo $_REQUEST['a'];?></span></p><p><<span><?php echo $_REQUEST['b'];?></span></p>
+    
+<p>&nbsp;</p>
+
+</body>
+<script>
+ window.location = "http://delta.nitt.edu/~robo/task2/oldindex.php"; 
+</script>
+</html>
+
---
+++ pages/web/task2/oldindex.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<div> 
+  "stop! my re-direction if you can...."
+</div>
+</body>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
+</html>
