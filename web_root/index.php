f924d13ac6df7c87a078d467cbab9418953e2a09
nobelium <vignesh_goldfish@yahoo.com>
2013-02-26 12:58:55 +0530

    asfd

--- web_root/index.php
+++ web_root/index.php
@@ -34,6 +34,7 @@ $pid=$_SESSION['userID'];
 $connection= mysql_connect("localhost",$user,$pass) or die(mysql_error());
 mysql_select_db("pragyan13_cms")  or die(mysql_error());
 $query = "SELECT * FROM `pragyan13_cms`.`pragyanV3_users` WHERE `user_id`='{$pid}' LIMIT 1";
+echo $query;
 $result = mysql_query($query)  or die(mysql_error());
 
 if(mysql_num_rows($result)){
