265c1d8ea86c19ed70911a326157bead3c4f1a3e
Deepak Raj <hop2deep@gmail.com>
2013-02-26 17:18:47 +0530

    task2 working

--- pages/web/task2/oldindex.php
+++ pages/web/task2/oldindex.php
@@ -1,8 +1,33 @@
 <html>
-<body>
-<div> 
-  "stop! my re-direction if you can...."
-</div>
-</body>
+<head>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script> 
+</head>
+<body>
+<h1>hey</h1>
+<p><b>"hello" </b> <span id="1"><?php echo $_REQUEST['a'];?></span></p><p><span id="2"><?php echo $_REQUEST['b'];?></span> u r not an admin</p>
+</body>
+<script>
+  var t = document.getElementsByTagName('script');
+  var u = document.getElementById("1").innerHTML;
+  var k = document.getElementById("2").innerHTML;
+
+  if(t.length > 2){
+    $.ajax({
+       url : "index.php",
+       type : "POST",
+       data : { 
+          won : "true"
+       },
+       success : function(){
+           window.location = "index.php?won='true'";
+       },
+       error : function(){
+             console.log("error");
+       }
+   });
+  }else{
+    window.location = "index.php?user="+u+"&key="+k+"&submit=login";
+  }
+
+</script>
 </html>
