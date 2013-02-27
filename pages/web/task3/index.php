18afe26e283946e22bf39044eebe068b23673786
paramesh waran <hsemarap@gmail.com>
2013-02-27 16:11:07 +0530

    task 3 web cookies

--- pages/web/task3/index.php
+++ pages/web/task3/index.php
@@ -3,14 +3,15 @@ session_start();
 if(isset($_POST['submit'])){
 	if($_FILES['file']['error']==0){
 		$filename = sha1(mt_rand());
-		$path = "tmp/".$filename.basename($_FILES["file"]["name"]);
+		$path = "/tmp/".$filename.basename($_FILES["file"]["name"]);
 		if(move_uploaded_file($_FILES['file']['tmp_name'], $path)){
 			$_COOKIE['user_url'] = "/".$path;
 			setcookie('user_url',$_COOKIE['user_url'],time()+600);
 			$_SESSION["cookie"]=array();
-			$_SESSION["cookie"]['user_url']=$user1;
+			$_SESSION["cookie"]['user_url']=$_COOKIE['user_url'];
 			$_SESSION["task3"]=1;
 		}
+		$ur=$path;
 	}
 	$url = "not found img url";
 }
@@ -21,6 +22,8 @@ if( isset($_SESSION['task3']) && !empty($_SESSION['task3']))
 //		   if($key=='username')$tempusername=$value;
 }
 
+print_r($_COOKIE);
+print_r($_SESSION);
 ?>
 
 <html>
